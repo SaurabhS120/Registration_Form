e46bf3616c894262e15dc932f69acde1f53ce584
Saurabh K. Sonar <saurabh.sonar120@gmail.com>
2022-04-07 17:48:10 +0530

    main test

--- app/src/androidTest/java/com/example/registrationform/MainTest.kt
+++ app/src/androidTest/java/com/example/registrationform/MainTest.kt
@@ -1,11 +1,15 @@
 package com.example.registrationform
 
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.anything
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -25,5 +29,29 @@ class MainTest {
         onView(withId(R.id.passwordEditText)).perform(scrollTo()).perform(typeText("aaaaaa1!"))
         onView(withId(R.id.confirmPasswordEditText)).perform(scrollTo()).perform(typeText("aaaaaa1!"))
         onView(withId(R.id.nextButton)).perform(scrollTo()).perform(click())
+
+        onView(withId(R.id.education_spinner)).perform(scrollTo()).perform(click())
+        onData(anything()).atPosition(1).perform(click())
+        onView(withId(R.id.passingYearSpinner)).perform(scrollTo()).perform(click())
+        onData(anything()).atPosition(1).perform(click())
+        onView(withId(R.id.gradeEditText)).perform(scrollTo()).perform(typeText("a"))
+        onView(withId(R.id.experienceEditText)).perform(scrollTo()).perform(typeText("1"))
+        onView(withId(R.id.designationEditText)).perform(scrollTo()).perform(typeText("a"))
+        onView(withId(R.id.domainEditText)).perform(scrollTo()).perform(typeText("a"))
+        onView(withId(R.id.nextButton)).perform(scrollTo()).perform(click())
+
+        onView(withId(R.id.address_edit_text)).perform(scrollTo()).perform(typeText("abc"))
+        onView(withId(R.id.landmark_edit_text)).perform(scrollTo()).perform(typeText("abc"))
+        onView(withId(R.id.city_edit_text)).perform(scrollTo()).perform(typeText("abc"))
+        onView(withId(R.id.state_spinner)).perform(scrollTo()).perform(click())
+        onData(anything()).atPosition(1).perform(click())
+        onView(withId(R.id.pin_code_edit_text)).perform(scrollTo()).perform(typeText("111111"))
+        onView(withId(R.id.nextButton)).perform(scrollTo()).perform(click())
+
+
+
+
+
+
     }
 }
