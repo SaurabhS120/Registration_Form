1f7cb278f2c41962f71c56b551dcb90d77fe36b1
Saurabh K. Sonar <saurabh.sonar120@gmail.com>
2022-04-09 20:20:22 +0530

    UserDetailsActivity data assign

--- app/src/main/java/com/example/registrationform/UserDetailsActivity.kt
+++ app/src/main/java/com/example/registrationform/UserDetailsActivity.kt
@@ -11,7 +11,29 @@ class UserDetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityUserDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val userDetailsData = UserDetailsData.createFromParcel(intent.extras!!.getParcelable("data")!!)
+        val userDetailsData = intent.extras!!.getParcelable<UserDetailsData>("user")!!
+
+        //basic details assign
+        binding.firstNameTextView.setText(userDetailsData.basicRegistrationDetailsData.firstName)
+        binding.lastNameTextView.setText(userDetailsData.basicRegistrationDetailsData.lastName)
+        binding.phoneNoTextView.setText(userDetailsData.basicRegistrationDetailsData.phoneNo)
+        binding.emailTextView.setText(userDetailsData.basicRegistrationDetailsData.email)
+        binding.genderTextView.setText(userDetailsData.basicRegistrationDetailsData.gender)
+
+        //education details assign
+        binding.educationDetailsTextView.setText(userDetailsData.educationDetailsData.education)
+        binding.passingYearTextView.setText(userDetailsData.educationDetailsData.year_of_passing)
+        binding.gradeTextView.setText(userDetailsData.educationDetailsData.grade)
+        binding.experienceTextView.setText(userDetailsData.educationDetailsData.experience)
+        binding.designationTextView.setText(userDetailsData.educationDetailsData.designation)
+        binding.domainTextView.setText(userDetailsData.educationDetailsData.domain)
+
+        //address details assign
+        binding.addressTextView.setText(userDetailsData.addressDetailsData.address)
+        binding.landmarkTextView.setText(userDetailsData.addressDetailsData.landmark)
+        binding.cityTextView.setText(userDetailsData.addressDetailsData.city)
+        binding.stateTextView.setText(userDetailsData.addressDetailsData.state)
+        binding.pinCodeTextView.setText(userDetailsData.addressDetailsData.pinCode)
 
     }
 }
