ae333af9e3cd0928f01effd0521962c4f9893455
Saurabh K. Sonar <saurabh.sonar120@gmail.com>
2022-04-25 18:10:22 +0530

    Location worker assigned

--- app/src/main/java/com/example/registrationform/MainActivity.kt
+++ app/src/main/java/com/example/registrationform/MainActivity.kt
@@ -6,16 +6,19 @@ import android.os.Bundle
 import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.registrationform.databinding.ActivityMainBinding
+import com.example.registrationform.location.LocationViewModel
 import com.example.registrationform.registration.data.UserDetailsData
 import com.example.registrationform.room.UsersDatabase
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
     lateinit var users:LiveData<List<UserDetailsData>>
+    lateinit var locationViewModel:LocationViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        locationViewModel = ViewModelFactory(this).create(LocationViewModel::class.java)
         binding.registerButton.setOnClickListener {
             startActivity(Intent(this,RegistrationActivity::class.java))
         }
@@ -26,6 +29,7 @@ class MainActivity : AppCompatActivity() {
         users.observe(this){
             adapter.updateUsers(it)
         }
+        locationViewModel.startLocationWorker(application)
 
     }
 }
