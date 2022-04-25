6df965496830395160274453c35de1f0ab177ac7
Saurabh K. Sonar <saurabh.sonar120@gmail.com>
2022-04-25 16:36:15 +0530

    LocationWorker.kt

---
+++ app/src/main/java/com/example/registrationform/location/LocationWorker.kt
@@ -0,0 +1,63 @@
+package com.example.registrationform.location
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.work.*
+import com.google.android.gms.common.internal.Constants
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+
+class LocationWorker(val appContext: Context, workerParams: WorkerParameters) : CoroutineWorker(appContext, workerParams) {
+    private var fusedLocationClient: FusedLocationProviderClient
+    init {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(appContext)
+    }
+    suspend fun getCurrentLocation(): Location? {
+        return withContext(Dispatchers.IO) {
+            lateinit var loc:Deferred<Location?>
+
+            loc.await()
+        }
+    }
+    override suspend fun doWork(): Result {
+        val resultChannel = Channel<Result>(1)
+        withContext(Dispatchers.IO) {
+            launch {
+                if (ActivityCompat.checkSelfPermission(
+                        appContext,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        appContext,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                        launch {
+                            val data = workDataOf(WorkerConstants.KEY_RESULT to location)
+                            val result = Result.success(data)
+                            resultChannel.send(result)
+                        }
+                    }
+                    fusedLocationClient.lastLocation.addOnFailureListener {
+                        launch{
+                            val data = workDataOf(WorkerConstants.KEY_ERROR to it)
+                            val result = Result.failure(data)
+                            resultChannel.send(result)
+                        }
+                    }
+                }
+            }
+        }
+        return withContext(Dispatchers.IO){
+            return@withContext resultChannel.receive()
+        }
+
+    }
+}
